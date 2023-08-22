67a781cbc43d7771c40b0df1fdc5aab20390149a
priya <ingolepriya02@gmail.com>
2023-08-22 22:50:02 +0530

    new add file sample1

---
+++ src/test/java/Library_File/sample.java
@@ -0,0 +1,14 @@
+package Library_File;
+
+public class sample 
+{
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("web automation");
+		System.out.println("Browser automation"); 
+		
+		
+	}
+
+}
